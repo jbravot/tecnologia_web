b49b7390855a120b1d1675bb0b28a35eeec188a1
Restrepo Vergara <huanre94@gmail.com>
2019-01-30 21:48:10 -0500

    CAMBIAR PATH DE DATAMONGO

--- LabMatec/LabMatec/settings.py
+++ LabMatec/LabMatec/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
     'default':{
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'TECNOLOGIA',
         'USER': 'admin',
         'PASSWORD': 'MSIGPROMOXX',
