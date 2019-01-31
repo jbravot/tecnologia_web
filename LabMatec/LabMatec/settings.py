7b29021d8f4b1cedbd1d468d57675b2a1eabb603
jbravot <jjbravo88@gmail.com>
2019-01-30 21:53:24 -0500

    [UPD] agregando nuevo diseño

--- LabMatec/LabMatec/settings.py
+++ LabMatec/LabMatec/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'main/static/')
